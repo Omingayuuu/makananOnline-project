e68834f9bf370b7328c6992194e0a9261efff38b
Ni Nyoman Ayu Lestari <113492364+Omingayuuu@users.noreply.github.com>
2023-04-17 15:54:01 +0800

    Create Admin.java

---
+++ Admin.java
@@ -0,0 +1,9 @@
+public class Admin {
+    public String nama;
+    public String email;
+    public String no_telp;
+    public String alamat;
+    public String password;
+    public String level;
+}
+
