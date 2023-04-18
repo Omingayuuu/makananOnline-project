cc93e5ad264de28b5f0300c38cb4913a8c622e10
Ni Nyoman Ayu Lestari <113492364+Omingayuuu@users.noreply.github.com>
2023-04-18 10:38:32 +0800

    Delete Admin.java

--- Admin.java
+++
@@ -1,9 +0,0 @@
-public class Admin {
-    public String nama;
-    public String email;
-    public String no_telp;
-    public String alamat;
-    public String password;
-    public String level;
-}
-
